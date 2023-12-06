d609d6271458dcfb313c0be18c886285ba2314b2
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-12-06 13:02:07 +0200

    Add files via upload

---
+++ zadacha19.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() { // Функція типу int
+    float x, y, r; // Декларування змінних типу float
+    bool ar1, ar2; // Декларування змінних типу bool
+    int n, count = 0;
+
+    cout << "Number of times: ";
+    cin >> n;
+
+    cout << "Enter the radius of the circle : ";
+    std::cin >> r;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        if (r > 0)
+        {
+            cout << "Enter a point by coordinate х : ";
+            cin >> x;
+            cout << "Enter a point by coordinate y : ";
+            cin >> y;
+
+
+            ar1 = pow((x - r), 2) + pow((y - r), 2) > pow(r, 2) && x < r && y < r && y>0 && x>0; //Рівняння першої фігури(верхньої)
+            ar2 = x > 0 && y < 0 && x<r && y>x - 2 * r; //Рівняння другої фігури(нижньої)
+
+            if (ar1 || ar2) {
+                cout << "In" << endl;
+                count++;
+            }
+            else {
+                cout << "Out" << endl;
+            }
+
+        }
+        else {
+            cout << "The radius cannot be negative" << endl;
+        }
+    }
+    cout << "Number of successful attempts: " << count << " / " << n;
+}
---
+++ zadacha22.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double calculateExpression(double x, int n) {
+    double result = log(-1 + cos(x));
+
+    for (int k = 1; k <= n; ++k) {
+        result -= pow(-1, k) / (k * pow(-1 + cos(x), k));
+    }
+
+    return result;
+}
+
+int main() {
+
+    // Example usage:
+    double x = 0;
+    int n = 0;
+
+    cout << "Enter x: ";
+    cin >> x;
+    cout << "Enter n: ";
+    cin >> n;
+
+    double result = calculateExpression(x, n);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
